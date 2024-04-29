50806afd568f86d9debf2ba890bdc322e292ead2
Olgayakubovich81 <olgale4@gmail.com>
2024-04-29 11:33:18 +0200

    Registration add

--- src/main/java/Registration.java
+++ src/main/java/Registration.java
@@ -78,8 +78,6 @@ public class Registration {
                 header.getText());
 
     }
-
-
     @Test
     public void withoutFirstName() {
         WebElement lastNameInputField =
@@ -97,6 +95,41 @@ public class Registration {
         assertEquals("Please fill in this field.",
                 firstNameInputField.getAttribute("validationMessage"));
 
+    }
+    @Test
+    public void withoutLastName() {
+        WebElement firstNameInputField =
+                driver.findElement(By.xpath("//input[@placeholder='Input your first name']"));
+        firstNameInputField.sendKeys("Tom");
+        WebElement emailInputField =
+                driver.findElement(By.cssSelector("[class=\"form-control third\"]"));
+        emailInputField.sendKeys("jblack@gmail.com");
+        WebElement submitButton =
+                driver.findElement(By.className("btn-default"));
+        submitButton.click();
+        //CHeck that error message is "Please fill in this field."
+        WebElement lastNameInputField =
+                driver.findElement(By.xpath("//input[@placeholder='Input your last name']"));
+        assertEquals("Please fill in this field.",
+                lastNameInputField.getAttribute("validationMessage"));
+
+    }
+    @Test
+    public void withoutEmail() {
+        WebElement firstNameInputField =
+                driver.findElement(By.xpath("//input[@placeholder='Input your first name']"));
+        firstNameInputField.sendKeys("Tom");
+        WebElement lastNameInputField =
+                driver.findElement(By.xpath("//input[@placeholder='Input your last name']"));
+        lastNameInputField.sendKeys("Black");
+        WebElement submitButton =
+                driver.findElement(By.className("btn-default"));
+        submitButton.click();
+        //CHeck that error message is "Please fill in this field."
+        WebElement emailInputField =
+                driver.findElement(By.cssSelector("[class=\"form-control third\"]"));
+        assertEquals("Please fill in this field.",
+                emailInputField.getAttribute("validationMessage"));
     }
 }
 
